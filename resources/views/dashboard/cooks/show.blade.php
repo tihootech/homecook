200b2ad15fc9e0d027b647772a321d4d473dfa23
Ali Seyfi <ali.seyfi68@gmail.com>
2020-03-12 22:42:40 +0330

    cook show in panel

--- resources/views/dashboard/cooks/show.blade.php
+++ resources/views/dashboard/cooks/show.blade.php
@@ -6,7 +6,24 @@
 
 	<div class="tile">
         <div class="container">
-            در دست ساخت
+            @if ($cook->balance())
+                <div class="row">
+                    <div class="col-md-6 mx-auto">
+                        @include('includes.payment_card', ['object'=>'cook'])
+                    </div>
+                </div>
+            @else
+                <div class="alert alert-info">
+                    با این همکار تسویه حساب شده است.
+                </div>
+            @endif
+            <hr>
+            <div class="text-center">
+                <a href="{{route('food.index')}}?cooks%5B%5D={{$cook->id}}" class="btn btn-primary">
+                    <i class="fa fa-list ml-1"></i>
+                    لیست غذا های این همکار
+                </a>
+            </div>
         </div>
     </div>
 
