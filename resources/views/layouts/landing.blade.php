5a9f3597eef32d636506d19ff01a4e5fd83a951b
tihootech <ali.seyfi68@gmail.com>
2020-06-23 14:03:21 +0430

    footer bg image

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -23,7 +23,7 @@
 
     <link rel="stylesheet" href="{{asset('assets/css/icomoon.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/select2.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css?v=1.4')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css?v=1.5')}}">
 	<link rel="stylesheet" href="{{asset('assets/css/navbar.css')}}">
 </head>
 
