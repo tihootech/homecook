059cd1c7257a6af65df249d46aa0f65d201d3040
tihootech <ali.seyfi68@gmail.com>
2020-07-06 07:48:05 +0430

    fix charset

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,6 +6,7 @@
 	@yield('meta')
 
     <meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
